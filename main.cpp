0b42060526a3100c0d7c8e4854648eda3b1a3dec
Didenko Yakov <kogotto@gmail.com>
2014-06-26 17:52:02 +0400

    test input with select sort

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
-
+#include <vector>
+#include <functional>
 using namespace std;
+
+template<class item_t, class comp_t>
+void heapSort(vector<item_t> & data, const comp_t & comp = comp_t())
+{
+    const size_t size = data.size();
+    for (size_t i = 0; i < size - 1; ++i) {
+        size_t minIndex = i;
+        for (size_t j = i + 1; j < size; ++j) {
+            if (comp(data[j], data[minIndex])) {
+                minIndex = j;
+            }
+        }
+        swap(data[i], data[minIndex]);
+    }
+}
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    vector<int> data;
+
+    size_t size = 0;
+    cin >> size;
+    for (size_t i = 0; i < size; ++i) {
+        int tmp = 0;
+        cin >> tmp;
+        data.push_back(tmp);
+    }
+
+    heapSort(data, less<int>());
+
+    for (size_t i = 0; i < data.size(); ++i) {
+        cout << data[i] << " ";
+    }
+
     return 0;
 }
 
