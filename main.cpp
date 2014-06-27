4b9f668a1f3657cc5420772f9aa7a05301c1795c
Didenko Yakov <kogotto@gmail.com>
2014-06-27 17:11:10 +0400

    complete task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <functional>
+#include <algorithm>
 using namespace std;
 
 
@@ -79,22 +80,86 @@ void heapSort(vector<item_t> & data, const comp_t & comp = comp_t())
 
 
 
+struct point_t {
+    explicit point_t(int x = 0, int y = 0):
+        x(x),
+        y(y)
+    {}
+
+    int x;
+    int y;
+};
+
+bool operator == (const point_t & left, const point_t & right) {
+    return (left.x == right.x) && (left.y == right.y);
+}
+
+
+
+double tan(const point_t & origin, const point_t & end) {
+    return (static_cast<double>(end.y - origin.y)) / (end.x - origin.x);
+}
+
+
+
+struct point_comparator_t {
+    point_comparator_t(const vector<point_t> & data) {
+        size_t min = 0;
+        for (size_t i = 1; i < data.size(); ++i) {
+            if ((data[min].x == data[i].x) &&
+                    (data[min].y > data[i].y)) {
+                min = i;
+                continue;
+            }
+
+            if (data[min].x > data[i].x) {
+                min = i;
+            }
+        }
+
+        first = data[min];
+    }
+
+    bool operator () (const point_t & left, const point_t & right) const {
+        if (left == first) {
+            return true;
+        }
+
+        if (right == first) {
+            return false;
+        }
+
+        return tan(first, right) < tan(first, left);
+    }
+
+private:
+    point_t first;
+};
+
+
+
 int main()
 {
-    vector<int> data;
+    vector<point_t> data;
 
     size_t size = 0;
     cin >> size;
-    for (size_t i = 0; i < size; ++i) {
-        int tmp = 0;
-        cin >> tmp;
-        data.push_back(tmp);
+    if (size == 0) {
+        return 0;
     }
 
-    heapSort(data, less<int>());
+    for (size_t i = 0; i < size; ++i) {
+        int x = 0;
+        cin >> x;
+        int y = 0;
+        cin >> y;
+        data.push_back(point_t(x, y));
+    }
+
+    heapSort(data, point_comparator_t(data));
 
     for (size_t i = 0; i < data.size(); ++i) {
-        cout << data[i] << " ";
+        cout << data[i].x << " " << data[i].y << endl;
     }
 
     return 0;
