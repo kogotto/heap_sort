e70ba188539b7510f7b2dabd1e01fc32b952da46
Didenko Yakov <kogotto@gmail.com>
2014-06-27 16:01:28 +0400

    refactoring

--- main.cpp
+++ main.cpp
@@ -3,26 +3,25 @@
 #include <functional>
 using namespace std;
 
-template<class item_t, class comp_t>
-void heapSort(vector<item_t> & data, const comp_t & comp = comp_t())
-{
-    const size_t size = data.size();
-    buildHeap(data, comp);
-    for (size_t i = 0; i < size - 1; ++i) {
-        swap(data[0], data[size - i - 1]);
-        heapify(data, 0, size - i - 1, comp);
-    }
+
+
+inline size_t leftChild(size_t current) {
+    return 2 * current + 1;
 }
 
-template<class item_t, class comp_t>
-void buildHeap(vector<item_t> & data, const comp_t & comp)
-{
-    const size_t size = data.size();
-    const size_t last = size - 1;
-    for (int i = parrent(last); i >= 0; --i) {
-        heapify(data, i, size, comp);
-    }
+
+
+inline size_t rightChild(size_t current) {
+    return leftChild(current) + 1;
 }
+
+
+
+inline size_t parrent(size_t current) {
+    return (current - 1) / 2;
+}
+
+
 
 template<class item_t, class comp_t>
 void heapify(vector<item_t> & data, size_t elem, size_t heapSize, const comp_t & comp) {
@@ -30,12 +29,12 @@ void heapify(vector<item_t> & data, size_t elem, size_t heapSize, const comp_t &
     for (;;) {
         size_t max = current;
 
-        const size_t left = left(current);
+        const size_t left = leftChild(current);
         if (left < heapSize && !comp(data[left], data[max])) {
             max = left;
         }
 
-        const size_t right = right(current);
+        const size_t right = rightChild(current);
         if (right < heapSize && !comp(data[right], data[max])) {
             max = right;
         }
@@ -49,17 +48,31 @@ void heapify(vector<item_t> & data, size_t elem, size_t heapSize, const comp_t &
     }
 }
 
-inline size_t left(size_t current) {
-    return 2 * current + 1;
+
+
+template<class item_t, class comp_t>
+void buildHeap(vector<item_t> & data, const comp_t & comp)
+{
+    const size_t size = data.size();
+    const size_t last = size - 1;
+    for (int i = parrent(last); i >= 0; --i) {
+        heapify(data, i, size, comp);
+    }
 }
 
-inline size_t right(size_t currnet) {
-    return left(currernt) + 1;
+
+
+template<class item_t, class comp_t>
+void heapSort(vector<item_t> & data, const comp_t & comp = comp_t())
+{
+    const size_t size = data.size();
+    buildHeap(data, comp);
+    for (size_t i = 0; i < size - 1; ++i) {
+        swap(data[0], data[size - i - 1]);
+        heapify(data, 0, size - i - 1, comp);
+    }
 }
 
-inline size_t parrent(size_t current) {
-    return (current - 1) / 2;
-}
 
 
 int main()
